656a292a70b0e8690aeba5e5a5856243812068c2
Masood Ahmed <me@ahmedmasood.com>
2025-10-29 14:34:21 -0400

    Configuring Netbird to use Zitadel.

--- services/netbird-server.nix
+++ services/netbird-server.nix
@@ -12,16 +12,22 @@
   domain = "mantannest.com";
   netbirdDomain = "netbird.${domain}";
   oidcDomain = "auth.${domain}";
-  clientId = "sr60iFh1hxJNFawnM5dPHEfl2LG4oyZ9xBsNXHmP";
+  oidcIssuer = "https://${oidcDomain}";
+  oidcEndpoint = "https://${oidcDomain}/.well-known/openid-configuration";
+  oidcManagementEndpoint = "${oidcIssuer}/management/v1";
+  oidcTokenEndpoint = "https://${oidcDomain}/oauth/v2/token";
+  clientId = "344377599402770511";
 in {
   sops.secrets = {
     "netbird-turn-password" = {
       owner = "turnserver";
       mode = "0400";
     };
-    "netbird-authentik-password" = {};
-    "netbird-authentik-client-secret" = {};
     "netbird-envirnoment-file" = {};
+    "netbird-data-store-encryption-key" = {};
+    "netbird-relay-secret" = {};
+    "netbird-relay-secret-container-file" = {};
+    "netbird-zitadel-client-secret" = {};
   };
 
   services.netbird.server = {
@@ -29,78 +35,10 @@ in {
     enableNginx = true;
     domain = netbirdDomain;
 
-    management = {
-      # package = pkgs-unstable.netbird-management;
+    coturn = {
       enable = true;
-      enableNginx = true;
       domain = netbirdDomain;
-      turnDomain = netbirdDomain;
-      dnsDomain = "dns.${netbirdDomain}";
-      singleAccountModeDomain = netbirdDomain;
-      disableSingleAccountMode = false;
-      oidcConfigEndpoint = "https://${oidcDomain}/application/o/netbird/.well-known/openid-configuration";
-
-      settings = {
-        TURNConfig = {
-          Turns = [
-            {
-              Proto = "udp";
-              URI = "turn:turn.${netbirdDomain}:3478";
-              Username = "netbird";
-              Password._secret = "${config.sops.secrets."netbird-turn-password".path}";
-            }
-          ];
-
-          Secret._secret = "${config.sops.secrets."netbird-turn-password".path}";
-        };
-
-        DataStoreEncryptionKey = null;
-
-        StoreConfig = {
-          Engine = "postgres";
-        };
-
-        HttpConfig = {
-          AuthAudience = clientId;
-          AuthUserIDClaim = "sub";
-          AuthIssuer = "https://${oidcDomain}/application/o/netbird/";
-          AuthKeysLocation = "https://${oidcDomain}/application/o/netbird/jwks/";
-        };
-
-        IdpManagerConfig = {
-          ManagerType = "authentik";
-          ClientConfig = {
-            Issuer = "https://${oidcDomain}/application/o/netbird/";
-            ClientID = clientId;
-            TokenEndpoint = "https://${oidcDomain}/application/o/token/";
-            ClientSecret._secret = "${config.sops.secrets."netbird-authentik-client-secret".path}";
-          };
-          ExtraConfig = {
-            Password._secret = "${config.sops.secrets."netbird-authentik-password".path}";
-            Username = "Netbird";
-          };
-        };
-
-        PKCEAuthorizationFlow.ProviderConfig = {
-          Audience = clientId;
-          ClientID = clientId;
-          ClientSecret._secret = "${config.sops.secrets."netbird-authentik-client-secret".path}";
-          Scope = "openid profile email offline_access api";
-          AuthorizationEndpoint = "https://${oidcDomain}/application/o/authorize/";
-          TokenEndpoint = "https://${oidcDomain}/application/o/token/";
-          RedirectURLs = [
-            "http://localhost:53000"
-          ];
-        };
-      };
-    };
-
-    signal = {
-      # package = pkgs-unstable.netbird-signal;
-      enable = true;
-      port = 10000;
-      domain = netbirdDomain;
-      enableNginx = true;
+      passwordFile = "${config.sops.secrets."netbird-turn-password".path}";
     };
 
     dashboard = {
@@ -110,24 +48,131 @@ in {
       domain = netbirdDomain;
       managementServer = "https://${netbirdDomain}";
       settings = {
-        AUTH_AUTHORITY = "https://${oidcDomain}/application/o/netbird/";
-        AUTH_SUPPORTED_SCOPES = "openid profile email offline_access api";
         AUTH_AUDIENCE = clientId;
+        AUTH_AUTHORITY = oidcIssuer;
         AUTH_CLIENT_ID = clientId;
-        USE_AUTH0 = "false";
+        AUTH_SUPPORTED_SCOPES = "openid profile email offline_access api";
+        USE_AUTH0 = false;
       };
     };
 
-    coturn = {
+    management = {
+      package = pkgs-unstable.netbird-management;
       enable = true;
-      passwordFile = "${config.sops.secrets."netbird-turn-password".path}";
+      enableNginx = true;
       domain = netbirdDomain;
+      turnDomain = netbirdDomain;
+      dnsDomain = "dns.${netbirdDomain}";
+      singleAccountModeDomain = netbirdDomain;
+      oidcConfigEndpoint = oidcEndpoint;
+
+      settings = {
+        DataStoreEncryptionKey._secret = config.sops.secrets."netbird-data-store-encryption-key".path;
+        
+        DeviceAuthorizationFlow = {
+          Provider = "hosted";
+
+          ProviderConfig = {
+            Audience = clientId;
+            ClientID = clientId;
+            Scope = "openid";
+          };
+        };
+
+        HttpConfig = {
+          AuthAudience = clientId;
+          AuthIssuer = oidcIssuer;
+          IdpSignKeyRefreshEnabled = true;
+          OIDCConfigEndpoint = oidcEndpoint;
+        };
+
+        IdpManagerConfig = {
+          ManagerType = "zitadel";
+
+          ClientConfig = {
+            ClientID = "netbird";
+            ClientSecret._secret = config.sops.secrets."netbird-zitadel-client-secret".path;
+            GrantType = "client_credentials";
+            Issuer = oidcIssuer;
+            TokenEndpoint = oidcTokenEndpoint;
+          };
+
+          ExtraConfig = {
+            ManagementEndpoint = oidcManagementEndpoint;
+          };
+        };
+
+        PKCEAuthorizationFlow.ProviderConfig = {
+          Audience = clientId;
+          ClientID = clientId;
+          Scope = "openid profile email offline_access api";
+          RedirectURLs = ["http://localhost:53000"];
+        };
+
+        Relay = {
+          Addresses = [ "rels://${netbirdDomain}:33080" ];
+          CredentialsTTL = "24h";
+          Secret._secret = config.sops.secrets."netbird-relay-secret".path;
+        };
+
+        Signal = {
+          Proto = "https";
+          URI = "${netbirdDomain}:443";
+        };
+
+        StoreConfig = {
+          Engine = "postgres";
+        };
+
+        TURNConfig = {
+          Secret._secret = config.sops.secrets."netbird-turn-password".path;
+          CredentialsTTL = "12h";
+          TimeBasedCredentials = false;
+          Turns = [
+            {
+              Password._secret = config.sops.secrets."netbird-turn-password".path;
+              Proto = "udp";
+              URI = "turn:${netbirdDomain}:3478";
+              Username = "netbird";
+            }
+          ];
+        };
+      };
     };
+
+    signal = {
+      package = pkgs-unstable.netbird-signal;
+      enable = true;
+      port = 10000;
+      domain = netbirdDomain;
+      enableNginx = true;
+    };
+  };
+
+  # Run the Netbird relay with TLS to allow relaying over TCP
+  virtualisation.oci-containers.containers.netbird-relay = {
+    image = "netbirdio/relay:latest";
+    ports = [
+      "33080:33080"
+    ];
+    volumes = [
+      "/var/lib/acme/${netbirdDomain}/:/certs:ro"
+    ];
+    environment = {
+      NB_LOG_LEVEL = "info";
+      NB_LISTEN_ADDRESS = ":33080";
+      NB_EXPOSED_ADDRESS = "rels://${netbirdDomain}:33080";
+      NB_TLS_CERT_FILE = "/certs/fullchain.pem";
+      NB_TLS_KEY_FILE = "/certs/key.pem";
+    };
+    environmentFiles = [
+      config.sops.secrets."netbird-relay-secret-container-file".path
+    ];
   };
 
   # Make the env available to the systemd service
   systemd.services.netbird-management.serviceConfig = {
-    EnvironmentFile = "${config.sops.secrets."netbird-envirnoment-file".path}";
+    EnvironmentFile = config.sops.secrets."netbird-envirnoment-file".path;
   };
 
   # Override ACME settings to get a cert
