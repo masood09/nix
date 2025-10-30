e2ca4a45dbd80b0ef628607391d8694c619088a3
Masood Ahmed <me@ahmedmasood.com>
2025-10-30 00:10:50 -0400

    Adding configuration for oci-peer-server

---
+++ machines/oci-peer-server/configuration.nix
@@ -0,0 +1,35 @@
+{
+  inputs,
+  outputs,
+  vars,
+  ...
+}: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    inputs.impermanence.nixosModules.impermanence
+
+    ./../../modules/nixos/oci-hardware-configuration.nix
+
+    ./../../modules/nixos/auto-update.nix
+    ./../../modules/nixos/base.nix
+    ./../../modules/nixos/remote-unlock.nix
+
+    ./../../services/netbird-client.nix
+    ./../../services/systemd-resolved.nix
+  ];
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs outputs vars;};
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users = {
+      ${vars.userName} = {
+        imports = [
+          ./../../modules/home-manager/base.nix
+        ];
+      };
+    };
+  };
+
+  networking.hostName = "oci-peer-server";
+}
