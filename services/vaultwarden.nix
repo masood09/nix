1f87e0e721f0ef91013e8e5e00bc3c5d56519ea9
Masood Ahmed <me@ahmedmasood.com>
2025-11-02 00:10:52 -0400

    Passing the remote ip from proxy to vaultwarden

--- services/vaultwarden.nix
+++ services/vaultwarden.nix
@@ -46,6 +46,11 @@
       locations."/" = {
         proxyPass = "http://${config.services.vaultwarden.config.ROCKET_ADDRESS}:${toString config.services.vaultwarden.config.ROCKET_PORT}";
         proxyWebsockets = true;
+        extraConfig = ''
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
       };
     };
   };
