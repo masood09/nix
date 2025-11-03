eb407c078cd19403150e567bd364eda7136c0671
Masood Ahmed <me@ahmedmasood.com>
2025-11-02 20:55:25 -0500

    Removing oci-peer-server from prometheus scapers

--- services/prometheus.nix
+++ services/prometheus.nix
@@ -12,7 +12,6 @@
               targets = [
                 "oci-auth-server.dns.headscale.mantannest.com:9100"
                 "oci-vpn-server.dns.headscale.mantannest.com:9100"
-                "oci-peer-server.dns.headscale.mantannest.com:9100"
                 "pve-server-1.dns.headscale.mantannest.com:9100"
                 "pve-server-container.dns.headscale.mantannest.com:9100"
                 "pve-server-monitoring.dns.headscale.mantannest.com:9100"
