11dc84ad8e00587c3f59ba78e9b5bfe20d6e8ef9
Masood Ahmed <me@ahmedmasood.com>
2025-11-04 13:33:36 -0500

    Forcing the remote build options

--- modules/nixos/distributed-builds-x86_64_linux.nix
+++ modules/nixos/distributed-builds-x86_64_linux.nix
@@ -1,21 +1,23 @@
 {
-  nix.distributedBuilds = true;
+  nix = {
+    distributedBuilds = true;
 
-  nix.settings = {
-    builders-use-substitutes = true;
+    settings = {
+      builders-use-substitutes = true;
+    };
+
+    buildMachines = [
+      {
+        hostName = "pve-nix-builder";
+        mandatoryFeatures = [ ];
+        maxJobs = 3;
+        protocol = "ssh-ng";
+        speedFactor = 6;
+        sshUser = "remotebuild";
+        sshKey = "/etc/ssh/ssh_host_ed25519_key";
+        system = "x86_64-linux";
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      }
+    ];
   };
-
-  nix.buildMachines = [
-    {
-      hostName = "pve-nix-builder";
-      mandatoryFeatures = [ ];
-      maxJobs = 3;
-      protocol = "ssh-ng";
-      speedFactor = 6;
-      sshUser = "remotebuild";
-      sshKey = "/etc/ssh/ssh_host_ed25519_key";
-      system = "x86_64-linux";
-      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-    }
-  ];
 }
