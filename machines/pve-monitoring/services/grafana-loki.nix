b3c57b8ac068593e9542e507d3fab84ebd5a204c
Masood Ahmed <me@ahmedmasood.com>
2025-11-05 16:51:03 -0500

    Using loki configuration from sops-secrets

--- machines/pve-monitoring/services/grafana-loki.nix
+++ machines/pve-monitoring/services/grafana-loki.nix
@@ -1,6 +1,6 @@
 { config, ... }: {
   sops.secrets = {
-    "loki-env" = {
+    "loki-config" = {
       owner = "loki";
       sopsFile = ./../../../secrets/pve-monitoring.yaml;
     };
@@ -10,60 +10,8 @@
     loki = {
       enable = true;
 
-      extraFlags = [
-        "-config.expand-env=true"
-      ];
-
-      configuration = {
-        auth_enabled = false;
-
-        server = {
-          http_listen_address = "127.0.0.1";
-        };
-
-        common = {
-          ring = {
-            instance_addr = "127.0.0.1";
-            kvstore.store = "inmemory";
-          };
-
-          replication_factor = 1;
-          path_prefix = "/var/lib/loki";
-        };
-
-        schema_config = {
-          configs = [
-            {
-              from = "2025-11-05";
-              store = "tsdb";
-              object_store = "s3";
-              schema = "v13";
-
-              index = {
-                prefix = "index_";
-                period = "24h";
-              };
-            }
-          ];
-        };
-
-        storage_config = {
-          tsdb_shipper = {
-            active_index_directory = "/var/lib/loki/index";
-            cache_location = "/var/lib/loki/index_cache";
-          };
-
-          aws = {
-            s3 = "\${S3_URL}";
-            s3forcepathstyle = true;
-          };
-        };
-      };
+      configFile = config.sops.secrets."loki-config".path;
     };
-  };
-
-  systemd.services.loki.serviceConfig = {
-    EnvironmentFile = config.sops.secrets."loki-env".path;
   };
 
   environment.persistence."/nix/persist" = {
