f248ee5b9c4a8356b8846591c1c647b4bc69c29f
Masood Ahmed <me@ahmedmasood.com>
2025-11-09 21:10:18 -0500

    Configuring oauth for Grafana

--- machines/pve-monitoring/services/grafana.nix
+++ machines/pve-monitoring/services/grafana.nix
@@ -1,4 +1,11 @@
 {config, ...}: {
+  sops.secrets = {
+    "grafana-authentik-client-secret" = {
+      owner = "grafana";
+      sopsFile = ./../../../secrets/pve-monitoring.yaml;
+    };
+  };
+
   services = {
     grafana = {
       enable = true;
@@ -18,12 +25,26 @@
       };
 
       settings = {
+        auth.signout_redirect_url = "https://auth.mantannest.com/application/o/grafana/end-session/";
+        auth.oauth_auto_login = true;
+
+        "auth.generic_oauth".name = "authentik";
+        "auth.generic_oauth".enabled = true;
+        "auth.generic_oauth".client_id = "0i7eoX2Og14H7lL4v3Mh8C2WhSoN2dLCiq7yJgb4";
+        "auth.generic_oauth".client_secret = "$__file{${config.sops.secrets."grafana-authentik-client-secret".path}}";
+        "auth.generic_oauth".scopes = "openid email profile";
+        "auth.generic_oauth".auth_url = "https://auth.mantannest.com/application/o/authorize/";
+        "auth.generic_oauth".token_url = "https://auth.mantannest.com/application/o/token/";
+        "auth.generic_oauth".api_url = "https://auth.mantannest.com/application/o/userinfo/";
+        "auth.generic_oauth".role_attribute_path = "contains(groups, 'Homelab Admins') && 'Admin' || 'Viewer'";
+
         analytics.reporting_enabled = false;
 
         server = {
           enforce_domain = true;
           enable_gzip = true;
           domain = "grafana.mantannest.com";
+          root_url = "https://grafana.mantannest.com/";
         };
       };
     };
