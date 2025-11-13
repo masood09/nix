205d680f40a70f83c1498476109f9d20d18aa2bb
Masood Ahmed <me@ahmedmasood.com>
2025-11-13 11:14:24 -0500

    Adding machine configuration for Arada

---
+++ machines/arada/hardware-configuration.nix
@@ -0,0 +1,103 @@
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+        "sdhci_pci"
+        "r8169"
+      ];
+      kernelModules = [
+        "nvme"
+        "r8169"
+      ];
+
+      preLVMCommands = ''
+        echo "Waiting for NVMe devices..."
+        udevadm settle
+        sleep 5
+      '';
+
+      luks.devices."cryptroot" = {
+        device = "/dev/nvme0n1p2";
+        preLVM = true;
+      };
+    };
+
+    kernel.sysctl = {
+      "net.ipv6.conf.all.disable_ipv6" = 1;
+      "net.ipv6.conf.default.disable_ipv6" = 1;
+      "net.ipv6.conf.lo.disable_ipv6" = 1;
+    };
+
+    kernelModules = ["kvm-intel"];
+    extraModulePackages = [];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = ["defaults" "size=4G" "mode=0755"];
+    };
+    "/boot" = {
+      device = "/dev/nvme0n1p1";
+      fsType = "vfat";
+      options = ["umask=0077"];
+      neededForBoot = true;
+    };
+    "/nix" = {
+      device = "/dev/mapper/cryptroot";
+      fsType = "ext4";
+    };
+  };
+
+  networking = {
+    hostName = "arada";
+    dhcpcd.enable = false;
+    useDHCP = false;
+    useNetworkd = true;
+    wireless.enable = false;
+    enableIPv6 = false;
+  };
+
+  systemd.network = {
+    enable = true;
+
+    networks = {
+      "10-enp1s0" = {
+        matchConfig.MACAddress = "84:47:09:46:ed:7d";
+        networkConfig.DHCP = "yes";
+
+        dhcpV4Config = {
+          UseRoutes = true;
+          UseDNS = true;
+        };
+      };
+      "20-enp2s0" = {
+        matchConfig.MACAddress = "84:47:09:46:ed:7f";
+        networkConfig.DHCP = "yes";
+
+        dhcpV4Config = {
+          UseRoutes = false;
+          UseDNS = false;
+        };
+      };
+    };
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
