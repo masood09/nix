b1c4b58dae377b4e3d435abb23d8fd4a97d25cf4
Masood Ahmed <me@ahmedmasood.com>
2025-11-21 14:49:38 -0500

    feat(homelab): Add Git configuration module
    Introduce a new Git configuration module to manage
    Git settings. This refactor improves organization
    by separating program-specific configurations into
    individual files.

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -5,6 +5,7 @@
 }: {
   imports = [
     ./networking
+    ./programs
   ];
 
   options.homelab = {
@@ -14,34 +15,6 @@
       description = ''
         Whether impermanence should be enabled.
       '';
-    };
-
-    programs = {
-      git = {
-        enable = lib.mkOption {
-          default = true;
-          type = lib.types.bool;
-          description = ''
-            Whether to enable git.
-          '';
-        };
-
-        userName = lib.mkOption {
-          default = "Masood Ahmed";
-          type = lib.types.str;
-          description = ''
-            The userName option for git.
-          '';
-        };
-
-        userEmail = lib.mkOption {
-          default = "me@ahmedmasood.com";
-          type = lib.types.str;
-          description = ''
-            The userEmail option for git.
-          '';
-        };
-      };
     };
 
     primaryUser = {
---
+++ modules/homelab/programs/_git.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  ...
+}: {
+  options.homelab = {
+    programs = {
+      git = {
+        enable = lib.mkOption {
+          default = true;
+          type = lib.types.bool;
+          description = ''
+            Whether to enable git.
+          '';
+        };
+
+        userName = lib.mkOption {
+          default = "Masood Ahmed";
+          type = lib.types.str;
+          description = ''
+            The userName option for git.
+          '';
+        };
+
+        userEmail = lib.mkOption {
+          default = "me@ahmedmasood.com";
+          type = lib.types.str;
+          description = ''
+            The userEmail option for git.
+          '';
+        };
+      };
+    };
+  };
+}
