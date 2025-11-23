be66bd3132a6280f889bf7fba894f8b5c408100e
Masood Ahmed <me@ahmedmasood.com>
2025-11-22 23:35:10 -0500

    feat(home-manager): Enable Zsh plugins and features
    Add autosuggestions, syntax highlighting, and
    vi-mode plugin to Zsh configuration.

--- modules/home-manager/programs/_zsh.nix
+++ modules/home-manager/programs/_zsh.nix
@@ -1,7 +1,22 @@
-{homelabCfg, ...}: {
+{
+  homelabCfg,
+  pkgs,
+  ...
+}: {
   programs = {
     zsh = {
       inherit (homelabCfg.programs.zsh) enable;
+
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      plugins = [
+        {
+          name = "vi-mode";
+          src = pkgs.zsh-vi-mode;
+          file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+        }
+      ];
     };
   };
 }
