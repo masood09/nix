7f130b2b5e7e5a928cfc98afc5a01c84e9b5cf59
Masood Ahmed <me@ahmedmasood.com>
2025-11-25 16:07:53 -0500

    feat(caddy): Enable Caddy service configuration and firewall ports
    Add support for enabling the Caddy service and
    configuring the allowed TCP ports in the firewall
    when the service is enabled.

--- modules/services/caddy/default.nix
+++ modules/services/caddy/default.nix
@@ -1,5 +1,16 @@
-{ config, lib, pkgs, ... }:
-
 {
-  
+  config,
+  lib,
+  ...
+}: let
+  caddyEnabled = config.homelab.services.caddy.enable;
+in {
+  services.caddy = {
+    enable = caddyEnabled;
+  };
+
+  networking.firewall.allowedTCPPorts = lib.mkIf caddyEnabled [
+    80
+    443
+  ];
 }
