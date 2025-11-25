f859215460b045ef1fc4b06a0ef40c17108a706e
Masood Ahmed <me@ahmedmasood.com>
2025-11-25 16:25:01 -0500

    feat(minio): Add ACME host configuration for Caddy
    Include support for ACME host configuration in the
    Minio service. This allows for automatic
    certificate management for the defined S3 and
    admin hosts when using Caddy as a reverse proxy.

--- modules/services/minio/default.nix
+++ modules/services/minio/default.nix
@@ -31,15 +31,29 @@ in {
     caddy = lib.mkIf caddyEnabled {
       virtualHosts = {
         "${minioCfg.s3Host}" = {
+          useACMEHost = minioCfg.s3Host;
           extraConfig = ''
             reverse_proxy http://127.0.0.1:${toString minioCfg.listenPort}
           '';
         };
 
         "${minioCfg.adminHost}" = lib.mkIf minioCfg.browser {
+          useACMEHost = minioCfg.adminHost;
           extraConfig = ''
             reverse_proxy http://127.0.0.1:${toString minioCfg.consolePort}
           '';
+        };
+      };
+    };
+  };
+
+  security = lib.mkIf (caddyEnabled && minioCfg.enable) {
+    acme = {
+      certs = {
+        "${minioCfg.s3Host}".domain = "${minioCfg.s3Host}";
+
+        "${minioCfg.adminHost}" = lib.mkIf minioCfg.browser {
+          domain = "${minioCfg.adminHost}";
         };
       };
     };
