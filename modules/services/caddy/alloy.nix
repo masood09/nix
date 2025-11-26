c581dc1e717ae31090bd22db6bbd3706690c131f
Masood Ahmed <me@ahmedmasood.com>
2025-11-25 20:32:40 -0500

    feat(caddy): Add logging configuration for Caddy service
    Implement file matching for Caddy log targets and
    configure Loki to scrape logs from specified
    paths. This feature improves monitoring and
    logging capabilities by forwarding logs to Grafana
    Loki.

--- modules/services/caddy/alloy.nix
+++ modules/services/caddy/alloy.nix
@@ -9,4 +9,10 @@ in {
   environment.etc."alloy/config-caddy.alloy" = lib.mkIf (caddyEnabled && alloyEnabled) {
     source = ./caddy.alloy;
   };
+
+  users = lib.mkIf(caddyEnabled && alloyEnabled) {
+    users.alloy = {
+      extraGroups = [config.services.caddy.group];
+    };
+  };
 }
