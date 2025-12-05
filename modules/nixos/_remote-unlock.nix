1e83eb15673a2e708968c44ad642fdbf5e976570
Masood Ahmed <me@ahmedmasood.com>
2025-12-05 11:14:26 -0500

    fix(nixos): Adjust SSH shell based on root configuration
    Modify the SSH shell to use
    `/bin/cryptsetup-askpass` only if the root
    filesystem is not ZFS, improving compatibility for
    different root configurations.

--- modules/nixos/_remote-unlock.nix
+++ modules/nixos/_remote-unlock.nix
@@ -6,14 +6,12 @@
   homelabCfg = config.homelab;
 in {
   boot = lib.mkIf homelabCfg.isEncryptedRoot {
-    kernelParams = ["ip=dhcp"];
-
     initrd.network = {
       enable = true;
 
       ssh = {
         enable = true;
-        shell = "/bin/cryptsetup-askpass";
+        shell = lib.mkIf (!homelabCfg.isRootZFS) "/bin/cryptsetup-askpass";
         authorizedKeys = config.users.users.${homelabCfg.primaryUser.userName}.openssh.authorizedKeys.keys;
         hostKeys = ["/nix/secret/initrd/ssh_host_ed25519_key"];
       };
