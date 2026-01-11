0684cb17f63c23d986ddec5906ff341014c77354
Masood Ahmed <me@ahmedmasood.com>
2026-01-10 22:13:43 -0500

    fix(hardware-configuration): Remove IPv6 sysctl settings
    Eliminate unnecessary sysctl configurations for
    IPv6 in hardware-configuration.nix to streamline
    setup.

--- machines/failsafeunit/hardware-configuration.nix
+++ machines/failsafeunit/hardware-configuration.nix
@@ -27,12 +27,6 @@
     extraModulePackages = [];
     supportedFilesystems = ["zfs"];
     zfs.extraPools = ["dpool"];
-
-    kernel.sysctl = {
-      "net.ipv6.conf.all.disable_ipv6" = 1;
-      "net.ipv6.conf.default.disable_ipv6" = 1;
-      "net.ipv6.conf.lo.disable_ipv6" = 1;
-    };
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
