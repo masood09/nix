795d57f6c50146d36248d59aaea86f771304a2d6
Masood Ahmed <me@ahmedmasood.com>
2026-01-17 18:00:48 -0500

    feat(zfs): Enable auto scrub and trim for managed datasets
    Add configuration for automatically scrubbing and
    trimming ZFS datasets if root ZFS is detected or
    any managed datasets are enabled. This enhances
    the maintenance of the ZFS file system in the
    homelab setup.

--- modules/services/zfs/default.nix
+++ modules/services/zfs/default.nix
@@ -1,5 +1,24 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  homelabCfg = config.homelab;
+
+  # Any datasets with enable = true?
+  anyManagedDatasets = (lib.attrNames (lib.filterAttrs (_: v: v.enable or false) config.homelab.zfs.datasets)) != [];
+in {
   imports = [
     ./dataset.nix
   ];
+
+  config = {
+    services = lib.mkIf (homelabCfg.isRootZFS || anyManagedDatasets) {
+      zfs = {
+        autoScrub.enable = true;
+        autoScrub.interval = "monthly";
+        trim.enable = true;
+      };
+    };
+  };
 }
