cff7a94f39bd31613b6d52b1528a5e0b56b4996d
Masood Ahmed <me@ahmedmasood.com>
2026-01-17 18:20:44 -0500

    feat(zfs): Add Alloy configuration for Prometheus integration
    Implement Alloy configuration to integrate ZFS
    datasets with Prometheus. This allows monitoring
    of ZFS metrics through the Prometheus exporter,
    conditional on the Alloy and ZFS service
    configurations.

---
+++ modules/services/zfs/alloy.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  homelabCfg = config.homelab;
+  alloyEnabled = homelabCfg.services.alloy.enable;
+
+  # Any datasets with enable = true?
+  anyManagedDatasets = (lib.attrNames (lib.filterAttrs (_: v: v.enable or false) config.homelab.zfs.datasets)) != [];
+
+  enableZFS = (homelabCfg.isRootZFS or false) || anyManagedDatasets;
+  zfsExporterPort = toString config.services.prometheus.exporters.zfs.port;
+in {
+  config = {
+    environment.etc."alloy/config-zfs.alloy" = lib.mkIf (enableZFS && alloyEnabled && config.services.prometheus.exporters.zfs.enable) {
+      text = ''
+        prometheus.scrape "zfs_target" {
+          targets = [
+            {
+              __address__ = "127.0.0.1:${zfsExporterPort}",
+              instance = sys.env("ALLOY_HOSTNAME"),
+            },
+          ]
+
+          forward_to = [prometheus.remote_write.metrics_service.receiver]
+        }
+      '';
+    };
+  };
+}
--- modules/services/zfs/default.nix
+++ modules/services/zfs/default.nix
@@ -7,17 +7,25 @@
 
   # Any datasets with enable = true?
   anyManagedDatasets = (lib.attrNames (lib.filterAttrs (_: v: v.enable or false) config.homelab.zfs.datasets)) != [];
+
+  enableZFS = (homelabCfg.isRootZFS or false) || anyManagedDatasets;
 in {
   imports = [
+    ./alloy.nix
     ./dataset.nix
   ];
 
-  config = {
+  config = lib.mkIf enableZFS {
     services = lib.mkIf (homelabCfg.isRootZFS || anyManagedDatasets) {
       zfs = {
         autoScrub.enable = true;
         autoScrub.interval = "monthly";
         trim.enable = true;
+      };
+
+      prometheus.exporters.zfs = lib.mkIf homelabCfg.services.alloy.enable {
+        enable = true;
+        listenAddress = "127.0.0.1";
       };
     };
   };
