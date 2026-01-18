51952134f48e1131e6ac87cc6a21dc4d00ca400a
Masood Ahmed <me@ahmedmasood.com>
2026-01-18 16:34:29 -0500

    refactor(homelab): Move primary user options to user module
    Consolidate primary user configuration into the
    user management module for better organization and
    maintainability.

--- config/homelab/default.nix
+++ config/homelab/default.nix
@@ -1,35 +1,7 @@
-{lib, ...}: {
+{
   imports = [
     ./networking
     ./programs
     ./services
   ];
-
-  options.homelab = {
-    primaryUser = {
-      userId = lib.mkOption {
-        default = 1000;
-        type = lib.types.int;
-        description = ''
-          User ID of the user
-        '';
-      };
-
-      userName = lib.mkOption {
-        default = "masoodahmed";
-        type = lib.types.str;
-        description = ''
-          Primary User of the system
-        '';
-      };
-
-      sshPublicKey = lib.mkOption {
-        default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfTOXZ6W+DhUQcytGQ1ob+eFPQwbyiTB8wXnRSiYqpK";
-        type = lib.types.str;
-        description = ''
-          Public SSH key to be added to authrorized keys
-        '';
-      };
-    };
-  };
 }
--- modules/nixos/_users.nix
+++ modules/nixos/_users.nix
@@ -1,33 +1,64 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: let
   homelabCfg = config.homelab;
 in {
-  users = {
-    defaultUserShell =
-      if homelabCfg.programs.zsh.enable or false
-      then pkgs.zsh
-      else pkgs.bash;
+  options.homelab = {
+    primaryUser = {
+      userId = lib.mkOption {
+        default = 1000;
+        type = lib.types.int;
+        description = ''
+          User ID of the user
+        '';
+      };
 
-    mutableUsers = false;
+      userName = lib.mkOption {
+        default = "masoodahmed";
+        type = lib.types.str;
+        description = ''
+          Primary User of the system
+        '';
+      };
 
+      sshPublicKey = lib.mkOption {
+        default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfTOXZ6W+DhUQcytGQ1ob+eFPQwbyiTB8wXnRSiYqpK";
+        type = lib.types.str;
+        description = ''
+          Public SSH key to be added to authrorized keys
+        '';
+      };
+    };
+  };
+
+  config = {
     users = {
-      ${homelabCfg.primaryUser.userName} = {
-        isNormalUser = true;
-        description = homelabCfg.primaryUser.userName;
+      defaultUserShell =
+        if homelabCfg.programs.zsh.enable or false
+        then pkgs.zsh
+        else pkgs.bash;
 
-        extraGroups = [
-          "networkmanager"
-          "wheel"
-        ];
+      mutableUsers = false;
 
-        openssh.authorizedKeys.keys = [
-          homelabCfg.primaryUser.sshPublicKey
-        ];
+      users = {
+        ${homelabCfg.primaryUser.userName} = {
+          isNormalUser = true;
+          description = homelabCfg.primaryUser.userName;
 
-        hashedPasswordFile = config.sops.secrets."user-password".path;
+          extraGroups = [
+            "networkmanager"
+            "wheel"
+          ];
+
+          openssh.authorizedKeys.keys = [
+            homelabCfg.primaryUser.sshPublicKey
+          ];
+
+          hashedPasswordFile = config.sops.secrets."user-password".path;
+        };
       };
     };
   };
