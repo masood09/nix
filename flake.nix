368d62fc72882338d42ed75f408a8f788d0ce3ba
Masood Ahmed <me@ahmedmasood.com>
2026-01-20 15:43:29 -0500

    chore(pve-database): remove not-needed pve-database configuration files
    Delete pve-database configuration and related
    service files as they are no longer needed in the
    project.

--- flake.nix
+++ flake.nix
@@ -134,7 +134,6 @@
       failsafeunit = mkNixOSConfig ./machines/failsafeunit;
       nastest = mkNixOSConfig ./machines/nastest;
 
-      pve-database = mkNixOSConfig ./machines/pve-database/configuration.nix;
       pve-monitoring = mkNixOSConfig ./machines/pve-monitoring/configuration.nix;
       pve-proxy = mkNixOSConfig ./machines/pve-proxy/configuration.nix;
 
