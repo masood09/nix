2f3837d60ca81cd311ed059c357b64e928596852
Masood Ahmed <me@ahmedmasood.com>
2026-01-29 18:05:47 -0500

    feat(zsh): Add compatibility handling for dumb terminals
    Implement checks in Zsh configuration to disable
    certain options and set a simple prompt when
    running in dumb terminal environments. This
    enhances usability by ensuring a basic interactive
    experience.

--- modules/home-manager/programs/_zsh.nix
+++ modules/home-manager/programs/_zsh.nix
@@ -1,10 +1,36 @@
-{homelabCfg, ...}: {
+{
+  homelabCfg,
+  lib,
+  ...
+}: {
   programs = {
     zsh = {
       inherit (homelabCfg.programs.zsh) enable;
 
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+
+      initContent = lib.mkOrder 500 ''
+        if [[ "$TERM" == "dumb" ]]
+        then
+          unsetopt zle
+          unsetopt prompt_cr
+          unsetopt prompt_subst
+
+          if whence -w precmd >/dev/null; then
+            unfunction precmd
+          fi
+
+          if whence -w preexec >/dev/null; then
+            unfunction preexec
+          fi
+
+          PS1='$ '
+          return
+        fi
+
+        [[ -o interactive ]] || return
+      '';
     };
   };
 }
