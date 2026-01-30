26150989666ebf7756ad18d040df4393e4a235a3
Masood Ahmed <me@ahmedmasood.com>
2026-01-30 12:40:16 -0500

    fix(home-manager): Update package lists for desktop role
    Move common packages to the desktop role and
    ensure all relevant packages are included for
    proper functionality.

--- modules/home-manager/programs/_packages.nix
+++ modules/home-manager/programs/_packages.nix
@@ -9,27 +9,27 @@ in {
   home = {
     packages = with pkgs;
       [
-        age
-        findutils
-        gnutar
-        sops
-        stow
-        wget
-        xz
+        # Packages that are global.
       ]
       ++ lib.optionals (role == "server") [
         # Packages to be installed only on servers
       ]
       ++ lib.optionals (role == "desktop") [
         # Packages to be installed only on desktops (laptops/macs)
+        age
         alejandra
+        findutils
+        gnutar
         go
         just
         jq
         oci-cli
         opentofu
         restic
+        sops
         statix
+        stow
+        xz
         awscli2
 
         # Fonts
