6a0282dd236a80add2d5a61773246b69a01f4ff3
Masood Ahmed <me@ahmedmasood.com>
2026-02-02 18:33:45 -0500

    feat(jobscraper): Move configuration options to separate file
    Separate the Job Scraper configuration options
    into a new file for better organization and
    maintainability. This change enhances the clarity
    of code structure by isolating
    configuration-related settings.

--- modules/services/jobscraper/default.nix
+++ modules/services/jobscraper/default.nix
@@ -8,24 +8,9 @@
   podmanEnabled = homelabCfg.services.podman.enable;
   caddyEnabled = config.services.caddy.enable;
 in {
-  options.homelab.services.jobscraper = {
-    enable = lib.mkEnableOption "Whether to enable Job Scraper.";
-
-    webDomain = lib.mkOption {
-      type = lib.types.str;
-      default = "jobscraper.${config.networking.domain}";
-    };
-
-    listenAddress = lib.mkOption {
-      default = "127.0.0.1";
-      type = lib.types.str;
-    };
-
-    listenPort = lib.mkOption {
-      default = 8902;
-      type = lib.types.port;
-    };
-  };
+  imports = [
+    ./options.nix
+  ];
 
   config = lib.mkIf (jobscraperCfg.enable && podmanEnabled) {
     virtualisation.oci-containers.containers.jobscraper = {
