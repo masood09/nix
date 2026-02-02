d093ed4413b5010feaa856c6fa2c12431468dcb8
Masood Ahmed <me@ahmedmasood.com>
2026-02-02 18:14:29 -0500

    refactor(grafana): Move Grafana options into separate file
    Organize Grafana configuration options by
    extracting them into a new options.nix file for
    better maintainability.

--- modules/services/grafana/default.nix
+++ modules/services/grafana/default.nix
@@ -12,60 +12,9 @@
 
   grafanaDataDir = lib.removeSuffix "/" (toString grafanaCfg.dataDir);
 in {
-  options.homelab.services.grafana = {
-    enable = lib.mkEnableOption "Whether to enable Grafana.";
-
-    webDomain = lib.mkOption {
-      type = lib.types.str;
-      default = "grafana.${config.networking.domain}";
-    };
-
-    dataDir = lib.mkOption {
-      type = lib.types.path;
-      default = "/var/lib/grafana/";
-    };
-
-    oauth = {
-      providerHost = lib.mkOption {
-        type = lib.types.str;
-        default = "auth.${config.networking.domain}";
-      };
-
-      clientId = lib.mkOption {
-        type = lib.types.str;
-        default = "0i7eoX2Og14H7lL4v3Mh8C2WhSoN2dLCiq7yJgb4";
-      };
-
-      scopes = lib.mkOption {
-        type = lib.types.str;
-        default = "openid email profile";
-      };
-
-      roleAttributePath = lib.mkOption {
-        type = lib.types.str;
-        default = "contains(groups, 'Homelab Admins') && 'Admin' || 'Viewer'";
-      };
-    };
-
-    zfs = {
-      enable = lib.mkEnableOption "Store Grafana dataDir on a ZFS dataset.";
-
-      dataset = lib.mkOption {
-        type = lib.types.str;
-        default = "dpool/tank/services/grafana";
-      };
-
-      properties = lib.mkOption {
-        type = lib.types.attrsOf lib.types.str;
-        default = {
-          logbias = "latency";
-          recordsize = "16K";
-          relatime = "off";
-          primarycache = "all";
-        };
-      };
-    };
-  };
+  imports = [
+    ./options.nix
+  ];
 
   config = lib.mkIf grafanaCfg.enable {
     # ZFS dataset for dataDir
@@ -209,7 +158,7 @@ in {
             Type = "oneshot";
             RemainAfterExit = true;
             ExecStart = ''
-              ${pkgs.coreutils}/bin/install -d -m 0700 -o grafana -g grafana ${grafanaDataDir}
+              ${pkgs.coreutils}/bin/chown -R grafana:grafana ${grafanaDataDir}
             '';
           };
         };
