371ed6c5504134b17185b21d9cd7100b40184f76
Masood Ahmed <me@ahmedmasood.com>
2026-02-08 16:57:55 -0500

    feat(matrix-synapse): Add ZFS dataset support for data and media
    Implement ZFS dataset configuration for Matrix
    Synapse, including properties for data and media
    directories. Enable restic backup options for both
    datasets.

--- modules/services/matrix-synapse/default.nix
+++ modules/services/matrix-synapse/default.nix
@@ -11,9 +11,64 @@ in {
   ];
 
   config = lib.mkIf cfg.enable {
+    homelab.zfs.datasets = lib.mkIf cfg.zfs.enable {
+      matrix-synapse = {
+        enable = true;
+
+        inherit (cfg.zfs.dataDir) dataset properties;
+
+        mountpoint = cfg.dataDir;
+        requiredBy = ["matrix-synapse.service"];
+
+        restic = {
+          enable = true;
+        };
+      };
+
+      matrix-synapse-media = {
+        enable = true;
+
+        inherit (cfg.zfs.mediaDir) dataset properties;
+
+        mountpoint = cfg.mediaDir;
+        requiredBy = ["matrix-synapse.service"];
+
+        restic = {
+          enable = true;
+        };
+      };
+    };
+
     services = {
       matrix-synapse = {
         enable = true;
+
+        inherit (cfg) dataDir;
+
+        settings = {
+          media_store_path = cfg.mediaDir;
+          server_name = cfg.serverName;
+          public_baseurl = cfg.webDomain;
+
+          listeners = [
+            {
+              port = cfg.listenPort;
+              bind_addresses = cfg.listenAddress;
+              type = "http";
+              tls = false;
+              x_forwarded = true;
+              resources = [
+                {
+                  names = [
+                    "client"
+                    "federation"
+                  ];
+                  compress = true;
+                }
+              ];
+            }
+          ];
+        };
       };
     };
   };
--- modules/services/matrix-synapse/options.nix
+++ modules/services/matrix-synapse/options.nix
@@ -1,5 +1,88 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  zfsDatasetOpts = lib.types.submodule (_: {
+    options = {
+      dataset = lib.mkOption {
+        type = lib.types.str;
+        description = "ZFS dataset name.";
+      };
+
+      properties = lib.mkOption {
+        type = lib.types.attrsOf lib.types.str;
+        default = {};
+        description = "ZFS properties to apply to the dataset.";
+      };
+    };
+  });
+in {
   options.homelab.services.matrix-synapse = {
     enable = lib.mkEnableOption "Whether to enable Matrix Synapse.";
+
+    dataDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/matrix-synapse";
+    };
+
+    mediaDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/matrix-synapse-media";
+    };
+
+    serverName = lib.mkOption {
+      type = lib.types.str;
+      default = config.networking.domain;
+    };
+
+    webDomain = lib.mkOption {
+      type = lib.types.str;
+      default = "https://matrix.${config.networking.domain}/";
+    };
+
+    listenAddress = lib.mkOption {
+      default = ["127.0.0.1"];
+      type = lib.types.listOf lib.types.str;
+    };
+
+    listenPort = lib.mkOption {
+      default = 8008;
+      type = lib.types.port;
+    };
+
+    zfs = {
+      enable = lib.mkEnableOption "Store Matrix Synapse data on ZFS dataset.";
+
+      dataDir = lib.mkOption {
+        type = zfsDatasetOpts;
+        default = {
+          dataset = "fpool/fast/services/matrix-synapse";
+          properties = {
+            atime = "off";
+            compression = "zstd";
+            recordsize = "16K";
+            logbias = "latency";
+            xattr = "sa";
+            acltype = "posixacl";
+            redundant_metadata = "most";
+          };
+        };
+      };
+
+      mediaDir = lib.mkOption {
+        type = zfsDatasetOpts;
+        default = {
+          dataset = "dpool/tank/services/matrix-synapse-media";
+          properties = {
+            atime = "off";
+            compression = "zstd";
+            recordsize = "1M";
+            logbias = "throughput";
+            redundant_metadata = "most";
+          };
+        };
+      };
+    };
   };
 }
