1c800af30ac7ca73f498ce17cd1eefee9a2233ed
Masood Ahmed <me@ahmedmasood.com>
2026-02-25 14:39:53 -0500

    feat(gpg): Add GPG support with SSH agent integration
    Implement GPG settings including SSH agent
    integration for Bash, Fish, and Zsh. Enable GPG
    agent service with necessary configurations.

--- machines/KQ1FYQWP3Q-masoodahmed/_packages.nix
+++ machines/KQ1FYQWP3Q-masoodahmed/_packages.nix
@@ -1,1 +1,9 @@
-{}
+{lib, ...}: {
+  homebrew = {
+    brews = lib.mkAfter [
+      "hopenpgp-tools" # Key validity checker and linter
+      "pinentry-mac"
+      "ykman" # Command line Yubikey configuration utility
+    ];
+  };
+}
