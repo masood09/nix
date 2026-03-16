99247d755dcd1bfb7a4ae542a7aa3798113de205
Masood Ahmed <me@ahmedmasood.com>
2026-03-16 12:30:50 -0400

    docs(babybuddy): Add descriptions to all undocumented options
    Add description fields to webDomain, dataDir, listenAddress,
    listenPort, userId, and groupId.

--- modules/services/babybuddy/options.nix
+++ modules/services/babybuddy/options.nix
@@ -9,31 +9,37 @@
     webDomain = lib.mkOption {
       type = lib.types.str;
       default = "babybuddy.${config.networking.domain}";
+      description = "Domain name for the Baby Buddy web interface.";
     };
 
     dataDir = lib.mkOption {
       type = lib.types.path;
       default = "/var/lib/babybuddy";
+      description = "Directory for Baby Buddy data storage.";
     };
 
     listenAddress = lib.mkOption {
       default = "127.0.0.1";
       type = lib.types.str;
+      description = "Address Baby Buddy listens on for HTTP requests.";
     };
 
     listenPort = lib.mkOption {
       default = 8903;
       type = lib.types.port;
+      description = "Port Baby Buddy listens on for HTTP requests.";
     };
 
     userId = lib.mkOption {
       default = 3004;
       type = lib.types.ints.u16;
+      description = "UID for the Baby Buddy service user.";
     };
 
     groupId = lib.mkOption {
       default = 3004;
       type = lib.types.ints.u16;
+      description = "GID for the Baby Buddy service group.";
     };
 
     zfs = {
