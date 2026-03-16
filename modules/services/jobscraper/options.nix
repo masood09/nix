a6d49f15765c0f170f6322e2074daa6f7120b409
Masood Ahmed <me@ahmedmasood.com>
2026-03-16 12:31:34 -0400

    docs(jobscraper): Add descriptions to all undocumented options
    Add description fields to webDomain, listenAddress, and listenPort.

--- modules/services/jobscraper/options.nix
+++ modules/services/jobscraper/options.nix
@@ -9,16 +9,19 @@
     webDomain = lib.mkOption {
       type = lib.types.str;
       default = "jobscraper.${config.networking.domain}";
+      description = "Domain name for the Job Scraper web interface.";
     };
 
     listenAddress = lib.mkOption {
       default = "127.0.0.1";
       type = lib.types.str;
+      description = "Address Job Scraper listens on for HTTP requests.";
     };
 
     listenPort = lib.mkOption {
       default = 8902;
       type = lib.types.port;
+      description = "Port Job Scraper listens on for HTTP requests.";
     };
   };
 }
