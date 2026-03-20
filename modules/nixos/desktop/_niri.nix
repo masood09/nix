f61542f2f916b8b9954c2b22f05acd0916a977f4
Masood Ahmed <me@ahmedmasood.com>
2026-03-20 12:50:29 -0400

    feat(nixos): Add fingerprint authentication option at login
    Enable fingerprint authentication at login by
    adding a configurable option in the greetd module.

--- modules/nixos/desktop/_niri.nix
+++ modules/nixos/desktop/_niri.nix
@@ -30,6 +30,8 @@ in {
           greetd = {
             # Auto-unlock GNOME Keyring on login via PAM
             enableGnomeKeyring = true;
+            # Enable fingerprint authentication at login
+            fprintAuth = config.homelab.hardware.fingerprint.enable;
           };
         };
       };
