4044df6f9c225b3d55f6834dc7a22d0cc81d2305
Masood Ahmed <me@ahmedmasood.com>
2026-03-25 12:37:37 -0400

    fix(renovate): enable version detection for Nix packages
    Extend Renovate regex to match `version =` in addition to `rev =`.
    Deduplicate mailarchiver's version/rev into a single let binding so
    Renovate has one update target instead of two that could drift.

--- nix/pkgs/mailarchiver/default.nix
+++ nix/pkgs/mailarchiver/default.nix
@@ -4,34 +4,33 @@
   buildDotnetModule,
   dotnetCorePackages,
   fetchFromGitHub,
-}:
-buildDotnetModule {
-  pname = "mail-archiver";
-
+}: let
   # renovate: datasource=github-releases depName=s1t5/mail-archiver versioning=semver
   version = "2602.2";
+in
+  buildDotnetModule {
+    pname = "mail-archiver";
+    inherit version;
 
-  src = fetchFromGitHub {
-    owner = "s1t5";
-    repo = "mail-archiver";
+    src = fetchFromGitHub {
+      owner = "s1t5";
+      repo = "mail-archiver";
+      rev = version;
+      sha256 = "1x4n0cwl1x5x02v7l64ys5d56i7wz15a45sgzjm2ny2vc3n95rff";
+    };
 
-    # renovate: datasource=github-releases depName=s1t5/mail-archiver versioning=semver
-    rev = "2602.2";
-    sha256 = "1x4n0cwl1x5x02v7l64ys5d56i7wz15a45sgzjm2ny2vc3n95rff";
-  };
+    # Prefer csproj over sln for reliability
+    projectFile = "MailArchiver.csproj";
 
-  # Prefer csproj over sln for reliability
-  projectFile = "MailArchiver.csproj";
+    nugetDeps = ./deps.json;
 
-  nugetDeps = ./deps.json;
+    dotnet-sdk = dotnetCorePackages.sdk_10_0;
+    dotnet-runtime = dotnetCorePackages.aspnetcore_10_0;
 
-  dotnet-sdk = dotnetCorePackages.sdk_10_0;
-  dotnet-runtime = dotnetCorePackages.aspnetcore_10_0;
-
-  meta = with lib; {
-    description = "Mail-Archiver: automated email archiving, search, export";
-    homepage = "https://github.com/s1t5/mail-archiver";
-    license = licenses.gpl3Only;
-    platforms = platforms.linux;
-  };
-}
+    meta = with lib; {
+      description = "Mail-Archiver: automated email archiving, search, export";
+      homepage = "https://github.com/s1t5/mail-archiver";
+      license = licenses.gpl3Only;
+      platforms = platforms.linux;
+    };
+  }
